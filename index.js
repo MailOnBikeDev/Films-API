5b5000ed2112c2e883589d161eb1b2a4ff0c84a5
Dan Colmenares <dacolmenares93@gmail.com>
2021-02-02 21:16:23 -0500

    Trabajando desde casa

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { urlencoded } = require("express");
 const express = require("express");
 
+const apiRouter = require("./routes/api");
+
 const app = express();
 
 require("./db");
@@ -8,9 +10,7 @@ require("./db");
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-	res.send("Hola Mundo");
-});
+app.use("/api", apiRouter);
 
 app.listen(3000, () => {
 	console.log("Servidor iniciado!");
